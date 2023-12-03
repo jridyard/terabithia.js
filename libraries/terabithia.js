4eafdeffe39e03933b41a8418390fa82414b5c9f
Joe Ridyard <joeridyard3@gmail.com>
2023-12-03 02:02:30 -0600

    Added support for defaults: get react props, trigger react events

--- libraries/terabithia.js
+++ libraries/terabithia.js
@@ -11,7 +11,7 @@
 // ↓↓ CORE ↓↓
 
 // This callback will handle receiving message from the PAGE script context and sending responses back TO the PAGE from CONTENT script context.
-var handleCommandFromContent = async (command) => {
+var handleCommandFromContent = async (command, body) => {
     switch (command) {
         default:
             console.log(yellowSolidBg, `${framework}: Handled unexpected command via handleCommandFromContent: `, command);
@@ -22,7 +22,7 @@ var handleCommandFromContent = async (command) => {
 
 
 // This callback will handle receiving message from the CONTENT script context and sending responses back TO the CONTENT from PAGE script context.
-var handleCommandFromPage = async (command) => {
+var handleCommandFromPage = async (command, body) => {
     switch (command) {
         case "writeMeAPoem":
             return "Roses are red, violets are blue, I'm a poem, how about you?";
@@ -184,6 +184,71 @@ function uuidv4() {
     return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c => (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16));
 };
 
+function getReactProp(element, prop) {
+    const reactProps = getReactProps(element);
+    return reactProps[prop];
+};
+
+function getReactProps(element) {
+    const reactPropKey = getReactPropKey(element);
+    const reactProps = element[reactPropKey];
+    return reactProps;
+};
+
+function getReactPropKey(element) {
+    return Object.keys(element).filter(key => key.startsWith('__reactProps'))[0];
+};
+
+async function getElementsReactProps(element) {
+    if (!element) {
+        console.log('\x1b[31m%s\x1b[0m', `${framework}: getElementsReactProps requires an element. The one passed was not found`);
+        return null;
+    };
+
+    const targetId = uuidv4();
+    element.setAttribute('data-terabithia-target', targetId);
+
+    const propsRequest = await sendToPage({command: "getReactProps", selector: `[data-terabithia-target="${targetId}"]`});
+    const propsResponse = propsRequest?.response;
+
+    console.log('propsResponse: ', propsResponse)
+
+    if (!propsResponse?.success) {
+        console.log('\x1b[31m%s\x1b[0m', 'Error in getElementsReactProps: ', propsResponse?.message);
+    };
+
+    element.removeAttribute('data-terabithia-target');
+
+    return propsResponse?.data;
+};
+
+async function triggerElementsReactEvent(element, event) {
+    if (!element) {
+        console.log('\x1b[31m%s\x1b[0m', `${framework}: triggerElementsReactEvent requires an element. The one passed was not found`);
+        return null;
+    };
+
+    const targetId = uuidv4();
+    element.setAttribute('data-terabithia-target', targetId);
+
+    const eventRequest = await sendToPage({
+        command: "triggerReactEvent",
+        selector: `[data-terabithia-target="${targetId}"]`,
+        event
+    });
+    const eventResponse = eventRequest?.response;
+
+    console.log('eventResponse: ', eventResponse)
+
+    if (!eventResponse?.success) {
+        console.log('\x1b[31m%s\x1b[0m', 'Error in triggerReactEvent: ', eventResponse?.message);
+    };
+
+    element.removeAttribute('data-terabithia-target');
+
+    return eventResponse?.data;
+};
+
 // console.log(blueText, `${framework}: Initializing BRIDGE <${BRIDGE_ID}>`);
 
 /*
@@ -198,7 +263,7 @@ async function sendToCounterpartContext(json, bridgeId=COUNTERPART_BRIDGE_ID) {
         const senderId = uuidv4();
     
         json.senderId = senderId;
-        json.context = context; // PAGE
+        json.context = context;
     
         const eventInfo = {
             detail: json
@@ -293,6 +358,65 @@ if (context === 'PAGE') {
         var unexpectedCommand = false;
 
         switch (command) {
+            case "triggerReactEvent":
+                console.log('triggerReactEvent: ', body)
+                var element = document.querySelector(body.selector);
+                var reactProps = element && getReactProps(element);
+                var eventHandler = reactProps?.[body.event];
+                if (!eventHandler) {
+                    var errorMsg;
+                    if (!element) errorMsg = `No element found with selector ${body.selector}.`;
+                    else if (!reactProps) errorMsg = `No react props found on element with selector ${body.selector}.`;
+                    else if (!eventHandler) errorMsg = `No <${body.event}> handler found on element with selector ${body.selector}.`;
+                    console.log(redText, errorMsg);
+                    response = {
+                        success: false,
+                        message: errorMsg
+                    };
+                }
+                else {
+                    try {
+                        eventHandler(new Event(""));
+                        response = {
+                            success: true,
+                            message: `Successfully triggered ${body.event} on element with selector ${body.selector}.`
+                        };
+                    }
+                    catch(err) {
+                        console.log(redText, `${framework}: Error triggering ${body.event} on element with selector ${body.selector}: `, err);
+                        response = {
+                            success: false,
+                            message: err.toString()
+                        };
+                    };
+                };
+                break;
+            case "getReactProps":
+                console.log('getReactProps: ', body)
+                var element = document.querySelector(body.selector);
+                if (element) {
+                    var reactProps = element && getReactProps(element);
+                    // You are not allowed to pass callbacks over the bridge, so we need to filter them out or else we would lose ALL the data
+                    var reactPropsNoCallbacks = Object.keys(reactProps).filter(key => typeof reactProps[key] !== 'function').reduce((obj, key) => {
+                        obj[key] = reactProps[key];
+                        return obj;
+                    }, {});
+                    response = {
+                        success: true,
+                        data: reactPropsNoCallbacks
+                    };
+                }
+                else {
+                    response = {
+                        success: false,
+                        message: `No element found with selector ${body.selector}.`
+                    };
+                };
+
+                break;
+            case "sayHello":
+                response = "Hello from the PAGE script context!";
+                break;
             default:
                 unexpectedCommand = true;
                 break;
@@ -341,7 +465,7 @@ if (context === 'PAGE') {
     */
     sendToContent({
         command: 'checkContentListening'
-    }).then((response) => {
+    }).then(() => {
         console.log(`%cTERABITHIA is WORKING for: ${UNIQUE_IDENTIFIER}`, "color: yellow; background-color: blue; padding: 5px; font-weight: 500;");
     });
 };
